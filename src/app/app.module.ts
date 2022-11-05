1a0ad96ee1144c07f8fe994060b0d7b5afdcde6f
jose.sandoval <jose.sandoval.tirado.contractor@bbva.com>
2022-11-04 19:02:58 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//import { AppRoutingModule } from './app-routing.module';
+import { APP_ROUTES } from './app.routes';
+import { PagesModule } from './pages/pages.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PageComponent } from './pages/page.component';
 import { RegisterComponent } from './login/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    DashboardComponent,
-    ProgressComponent,
-    PageComponent,
     RegisterComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //AppRoutingModule,
+    APP_ROUTES,
+    PagesModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
